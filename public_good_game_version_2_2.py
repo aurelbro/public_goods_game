8be2030feac15bc5df1f99bec5f700efb9a67aad
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-04-20 12:22:44 +0200

    number of generation to 1000

--- public_good_game_version_2_2.py
+++ public_good_game_version_2_2.py
@@ -18,7 +18,7 @@ nI = 5
 S = [0, 1]                    # set of strategies
 fc = 0.5
 M = 0                          # necessary threshold for the benefit being shared
-number_of_generations = 100000
+number_of_generations = 1000
 
 # importations
 from random import *
