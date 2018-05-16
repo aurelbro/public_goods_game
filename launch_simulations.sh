19a323d8fb7ff6c72e22206235b196b559bece51
Aurelien <aurelien@octopus.istc.cnr.it>
2018-05-16 13:39:20 +0200

    change r to floats

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -5,7 +5,7 @@ mkdir simulations;
       for Beta in 10; do
         for fc in 0.1 0.3 0.5 0.7 0.9; do
           for M in 0 4 10; do 
-              python public_good_game_version_4.py simulations/ $r $mu $Beta $fc $M
+              python2 public_good_game_version_4.py simulations/ $r $mu $Beta $fc $M
             done
           done
         done
