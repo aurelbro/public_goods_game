02cb526cebc5d9b57dc27c53b8efa2fd67fb5eb7
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-05-17 12:31:54 +0200

    change the number of generartions

--- launch_plot.py
+++ launch_plot.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 import os
-number_of_generations = 100000
+number_of_generations = 6000
 
 
 def usage():
@@ -23,9 +23,9 @@ folder_1 = sys.argv[1]
 
 for folder in os.listdir(folder_1):
     if (os.path.exists(folder_1 + "/" + folder + "/merge_result.dat")):
-       os.remove(folder_1 + "/" + folder + "/merge_result.dat")
+        os.remove(folder_1 + "/" + folder + "/merge_result.dat")
     if (os.path.exists(folder_1 + "/" + folder + "/graphical_representation.pdf")):
-       os.remove(folder_1 + "/" + folder + "/graphical_representation.pdf")
+        os.remove(folder_1 + "/" + folder + "/graphical_representation.pdf")
     plt.figure(figsize=(20, 10), dpi=150)
     complete_list = []
     for file in os.listdir(folder_1 + "/" + folder):
