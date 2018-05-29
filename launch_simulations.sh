37b818b808d087041c6c3e94fe34fa3ea16bffd3
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-05-29 10:52:38 +0200

    change values of M

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -4,7 +4,7 @@ mkdir simulations_2;
     for mu in 0.0 0.01 ; do
       for Beta in 1; do
         for fc in 0.1 0.3 0.5 0.7 0.9; do
-          for M in 2 6 8; do 
+          for M in 0 4 10; do 
               python public_good_game_version_4.py simulations_2/ $r $mu $Beta $fc $M
             done
           done
