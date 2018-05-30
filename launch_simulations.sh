5323d8a3080582846b02f321056c1cf720394ec0
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-05-30 19:15:11 +0200

    some change

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mkdir simulations_2;
+mkdir simulations_3;
   for r in 8. 12.; do
     for mu in 0.0 0.01 ; do
-      for Beta in 1; do
+      for Beta in 1 10; do
         for fc in 0.1 0.3 0.5 0.7 0.9; do
-          for M in 0 4 10; do 
-              python public_good_game_version_4.py simulations_2/ $r $mu $Beta $fc $M
+          for M in 0 2 4 6 8 10; do 
+              python public_good_game_version_4.py simulations_3/ $r $mu $Beta $fc $M
             done
           done
         done
--- plot.py
+++ plot.py
@@ -1,4 +1,6 @@
 import csv
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
