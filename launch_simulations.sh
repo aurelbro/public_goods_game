958bd75c9cc8a591fa82e24dbd980b6ac6ede927
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-06-12 11:40:07 +0200

    try to add the seed

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-seed=$1
+#seed=$1
 
-mkdir simulations_with_leadership_1;
+mkdir simulations_4;
   for r in 3. 5. 7.; do
     for mu in 0.0  ; do
       for Beta in 10; do
         for fc in 0.1 0.3 0.5 0.7 0.9; do
-          for M in 0 4 10; do 
-              python public_good_game_with_leadership.py simulations_with_leadership_1/ $r $mu $Beta $fc $M $seed
+          for M in 0 4 10; do
+            for seed in $1; do 
+              python public_good_game_version_4.py simulations_4/ $r $mu $Beta $fc $M $seed
             done
           done
         done
       done
     done
+  done
