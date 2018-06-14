e1d4ab319b24ffa3dfc722000ac0a6e226c1dd2d
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-06-14 12:59:45 +0200

    file name

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -8,7 +8,7 @@ mkdir simulations_with_leadership_normal/;
       for Beta in 10; do
         for fc in 0.1 0.3 0.5 0.7 0.9; do
           for M in 0 4 10; do
-              python public_good_game_with_leadership_normal.py simulations_with_leadership_nomal/ $r $mu $Beta $fc $M $seed
+              python public_good_game_with_leadership_normal.py simulations_with_leadership_normal/ $r $mu $Beta $fc $M $seed
             done
           done
         done
