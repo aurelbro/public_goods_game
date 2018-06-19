10be4f4989072c44d4518845c78042fa9cd8de62
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-06-19 12:57:17 +0200

    include partitions per strenght

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -2,16 +2,17 @@
 
 seed=$1
 echo $seed
-mkdir simulations_with_leadership_exponential_2/;
+mkdir simulations_with_leadership_exponential_advanced/;
   for r in 3. 5. 7.; do
     for mu in 0.0  ; do
-      for Beta in 10; do
-        for fc in 0.1 0.3 0.5 0.7 0.9; do
-          for M in 0 4 10; do
-              python public_good_game_with_leadership_exponential.py simulations_with_leadership_exponential_2/ $r $mu $Beta $fc $M $seed
+      for Beta_imit in 10.; do
+        for Beta_follow in 0.1 1. do
+          for fc in 0.1 0.3 0.5 0.7 0.9; do
+            for M in 0 4 10; do
+                python public_good_game_with_leadership_exponential_advanced.py simulations_with_leadership_exponential_advanced/ $r $mu $Beta $fc $M $seed
             done
           done
         done
       done
     done
-
+  done
--- public_good_game_with_leadership_exponential_advanced.py
+++ public_good_game_with_leadership_exponential_advanced.py
@@ -36,7 +36,7 @@ def usage():
 
 
 numberOfArgs = len(sys.argv)
-if(numberOfArgs < 8):
+if(numberOfArgs < 7):
     usage()
     exit(-1)
 
