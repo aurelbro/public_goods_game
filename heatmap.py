ca6bb244282bc4bffe93b5eeacecca33f05cd858
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-06-22 17:35:42 +0200

    sdf

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,9 @@
 import numpy as np
-np.random.seed(0)
-import seaborn as sns
-sns.set()
+import os
+import csv
+#np.random.seed(0)
+#import seaborn as sns
+#sns.set()
 # uniform_data = np.random.rand(10, 12)
 # ax = sns.heatmap(uniform_data)
 # data = [[][]
@@ -14,11 +16,57 @@ sns.set()
 #        line = map(float, row)
 #       data.append(line)
 
-import plotly.plotly as py
-import plotly.graph_objs as go
+#import plotly.plotly as py
+#import plotly.graph_objs as go
 
-trace = go.Heatmap(z=[[1, 20, 30],
-                      [20, 1, 60],
-                      [30, 60, 1]])
-data = [trace]
-py.plot(data, filename='basic-heatmap')
+#trace = go.Heatmap(z=[[1, 20, 30],
+#                      [20, 1, 60],
+#                      [30, 60, 1]])
+#data = [trace]
+#py.plot(data, filename='basic-heatmap')
+
+def heatmap(r_par, mu_par, beta_imit, beta_follow, fc_par, M_par):
+    foldername = "simulations_with_leadership_exponential_advanced/r=%02d_mu=%.2f_Beta_imit=%.1f_Beta_follow=%.1f_fc=%.2f_M=%02d" % (r_par,mu_par, beta_imit, beta_follow, fc_par, M_par)
+    mat=np.zeros((6000, 5))
+    for file in os.listdir(foldername):
+        if file.endswith(".tsv"):
+            with open(foldername + "/" + file, "r") as file:
+                reader=csv.reader(file, delimiter='\t')
+                row=next(reader)
+                line=[0]*6000
+                #print(row[0]) 
+                #print(type(row[0]))               
+                #line=[float(i) for i in row[0]]
+                for i in range(6000):
+                  a=row[i].strip('[]')
+                  a=a.split(' ')
+                #if ('' in line):
+                  while ('' in a):
+                    a.remove('')
+                #line=[i in line if i!="''"]
+                #print(type(line))
+                #line=line.split(']')
+                #line=map(list, row[0])
+                #print(line)
+                  line[i]=map(float, a)
+                matrix=np.zeros(600,5)
+                for j in range(600):
+                  for k in range(5):
+                      matrix[j][k]=np.mean(line[100*j:100*(j+1)],axis=k)
+                #print(line)
+                #print(line[0])
+                #line=list(line)
+                #print(line)
+                
+                #print(row[1])
+                #cats = []
+                #the line below appends AND casts data from the csv
+                #cats = [map(float, c[0:]) for c in row]
+                #print(cats)
+                #line = map(float,row)
+                #for i in range(len(row)):
+                 #   for j in range(5):
+                #      mat[i][j]+= row[i][j]
+    #return mat
+
+heatmap(7,0.00,10.0,0.1,0.70,0)
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 import numpy as np
 a=np.array([[2.,3.,2.],[4.,5.,6.]])
 b=np.array([[.2,.35,.64], [1.3,6.2,9.845]])
-print(a[0])
+#print(a[0])
 #print(a!=b)
 #print(a[:,0])
 
@@ -14,14 +14,16 @@ C=[ A[i] for i in range(len(A)) if (A[i]==1) ]
 import numpy as np
 B=np.array([1.,2.5,6.3,4.4])
 C=np.array([1.,2.5,6.3,4.4])
-print(B/C)
+#print(B/C)
 
 #from scipy.stats import binom
 #prob = binom.cdf(x, n, p)
 #print(prob)
 
 #print(2<=5<=4)
-a=[[2.,3.,2.],[4,5,6]]
+a=[[2.,3.,2.],[4,5,6],[7,8,9]]
 b=deepcopy(a)
-print(a[0]!=b[0])
-print(a/b)
+#print(a[0]!=b[0])
+#print(a/b)
+
+print(a[0:2][:1])
