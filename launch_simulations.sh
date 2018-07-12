6b201e2dc6f0d41632350e17a7991de8b9f7b6d0
aurelbro <aurelien.brouillaud@polytechnique.edu>
2018-07-12 16:13:52 +0200

    rm mu

--- launch_simulations.sh
+++ launch_simulations.sh
@@ -2,17 +2,13 @@
 
 seed=$1
 echo $seed
-mkdir simulations_with_leadership_and_strength_mutation/;
+mkdir simulations_with_leadership_exponential_roulette_wheel_death/;
   for r in 3. 5. 7.; do
-    for mu in 0.0  ; do
-      for Beta_imit in 10.; do
         for Beta_follow in  1. ; do
           for fc in 0.1 0.3 0.5 0.7 0.9; do
             for M in 0 4 10; do
-                python public_good_game_with_leadership_and_strength_mutation.py simulations_with_leadership_and_strength_mutation/ $r $mu $Beta_imit $Beta_follow $fc $M $seed
+               python public_good_game_with_leadership_exponential_roulette_wheel_death.py simulations_with_leadership_exponential_roulette_wheel_death/ $r $Beta_follow $fc $M $seed
             done
           done
         done
-      done
-    done
   done
